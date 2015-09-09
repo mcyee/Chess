e8a38ffde55964af8fc2c4438e462ea529c5cb7e
Aishwary Gupta <guptashark@gmail.com>
2015-09-09 16:50:11 -0400

    Change the run function in gamebook to be a small command interpreter.

--- gamebook.cc
+++ gamebook.cc
@@ -31,8 +31,40 @@ GameBook::GameBook(string gamesFile): gamesFile(gamesFile) {
 
 void GameBook::run() {
 	cout << "This will have more capabilities, but right now it will just print this." << endl;
-	cout << "And display all the current games." << endl;
-	displayGames();
+	cout << "And display all the current games. Type in \"help\" for more options." << endl;
+	string input;
+	cout << "~> ";
+	string command;
+	while(getline(cin, input)) {
+		stringstream ss(input);
+		ss >> command;
+		if(command == "q" || command == "quit") {
+			return;
+		} else if(command == "open") {
+			string file;
+			ss >> file;
+			cout << "opening " << file << endl;
+			// initialize the controller and such. 
+			cout << "closing " << file << endl;
+		} else if(command == "new") {
+			// run the newgame function which will initialize the players
+			// and the played moves. Thsi should be done half in the 
+			// GameBook file and half ain the controller file. 
+			// Ie, make a dedicated "record main moves" function in 
+			// Controller that records the moves that were played, 
+			// jumps to the start node and then calls traverse so that 
+			// the player can analyze the game. 
+			cout << "tried to initialize a new game." << endl;
+		} else if(command == "help") {
+			// run the help function.
+			cout << "quit, open, new, help, display." << endl;
+		} else if(command == "display") {
+			displayGames();
+		} else {
+			cout << "unrecognized command." << endl;
+		}
+		cout << "~> ";
+	}
 }
 
 void GameBook::displayGames() {
